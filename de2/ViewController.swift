d38b12b58aeab64f6c6ee103202ea53d6ac430ca
gyseliya <gyseliya@mail.ru>
2021-09-20 10:02:43 +0300

    Initial Commit

---
+++ de2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  de2
+//
+//  Created by user on 20.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
